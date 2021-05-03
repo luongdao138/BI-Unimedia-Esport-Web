2a251359ed4f320354527588a045f1ce3c0a68a4
Tsengel <enkhbat.tsengel@gmail.com>
2021-05-03 09:23:31 +0800

    add esbutton

--- pages/atoms.tsx
+++ pages/atoms.tsx
@@ -1,37 +1,14 @@
-import { Button, Box } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-const useStyles = makeStyles(() => ({
-  // MuiButton-containedPrimary MuiButton-containedSizeSmall
-  containedPrimary: {
-    // background: theme.palette.primary,
-    '& .MuiButton-containedSizeSmall': {
-      color: 'red',
-    },
-  },
-}))
+import { Box } from '@material-ui/core'
+import ESButton from '@components/Button'
 
 const Atoms: React.FC = () => {
-  const classes = useStyles()
   return (
     <Box margin={4}>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" color="primary" disabled>
+      <ESButton variant="contained">Primary</ESButton>
+      <ESButton variant="contained" disabled>
         Primary Disabled
-      </Button>
-      <Button variant="outlined">Outlined</Button>
-      <Button variant="outlined" size="small" classes={classes}>
-        Outlined Small
-      </Button>
-      <Button
-        variant="contained"
-        size="small"
-        color="primary"
-        classes={classes}
-      >
-        Outlined Small
-      </Button>
+      </ESButton>
+      <ESButton variant="outlined">Outlined</ESButton>
     </Box>
   )
 }
