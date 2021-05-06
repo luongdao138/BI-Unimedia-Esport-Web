ed1d4e9efb9f82d05aad6969ddb9b2bdd5dcbed2
bayr-magnai <baldangombo_bayarmagnai@unimedia.co.jp>
2021-05-06 11:33:47 +0800

    page init

---
+++ pages/game-select/index.tsx
@@ -0,0 +1,7 @@
+import GameSelectContainer from '@containers/GameSelect'
+
+const GameSelectPage: React.FC = () => {
+  return <GameSelectContainer />
+}
+
+export default GameSelectPage
---
+++ src/containers/GameSelect/index.tsx
@@ -0,0 +1,15 @@
+import { Grid } from '@material-ui/core'
+
+const GameSelectContainer: React.FC = () => {
+  return (
+    <>
+      <Grid container>
+        <Grid container direction="row" justify="center">
+          <div>hello world</div>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default GameSelectContainer
