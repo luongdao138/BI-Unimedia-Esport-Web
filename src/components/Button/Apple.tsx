a03c395d7777e854b67c2ad4f86b5ae0ee456950
Tsengel <enkhbat.tsengel@gmail.com>
2021-05-08 06:25:45 +0800

    add apple login

--- src/components/Button/Apple.tsx
+++ src/components/Button/Apple.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next'
 import { Colors } from '@theme/colors'
 import { makeStyles } from '@material-ui/core/styles'
 import Icon from '@material-ui/core/Icon'
+import Script from 'react-load-script'
+import { LoginSocialParams } from '@services/auth.service'
 
 const useStyles = makeStyles((theme) => ({
   contained: {
@@ -64,4 +66,60 @@ ESButtonApple.defaultProps = {
   variant: 'contained',
 }
 
-export default ESButtonApple
+type AppleButtonProps = { onSuccess?: (param: LoginSocialParams) => void } & ButtonProps
+
+declare global {
+  interface Window {
+    AppleID: {
+      auth: {
+        init: ({
+          clientId,
+          scope,
+          redirectURI,
+          usePopup,
+        }: {
+          clientId: string
+          scope: string
+          redirectURI: string
+          usePopup: boolean
+        }) => void
+        signIn: () => Promise<{
+          authorization: {
+            id_token: string
+          }
+        }>
+      }
+    }
+  }
+}
+
+const AppleButton: React.FC<AppleButtonProps> = ({ onSuccess, ...rest }) => {
+  const handleLoadScript = () => {
+    window.AppleID.auth.init({
+      clientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+      scope: 'name email',
+      redirectURI: process.env.NEXT_PUBLIC_APPLE_CALLBACK,
+      usePopup: true,
+    })
+  }
+  const handleLogin = async () => {
+    try {
+      const { authorization } = await window.AppleID.auth.signIn()
+      !!onSuccess && onSuccess({ social_channel: 'apple', access_token: authorization.id_token })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  return (
+    <>
+      <ESButtonApple onClick={handleLogin} {...rest} />
+      <Script
+        url="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
+        onError={console.error}
+        onLoad={handleLoadScript}
+      />
+    </>
+  )
+}
+
+export default AppleButton
