d8822525f212223720214b31526b2e296d0ee949
Tsengel <enkhbat.tsengel@gmail.com>
2021-05-09 14:31:09 +0800

    add lineLogin

--- src/utils/helpers/CommonHelper.ts
+++ src/utils/helpers/CommonHelper.ts
@@ -3,7 +3,9 @@ const validateEmail = (email: string): boolean => {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
 }
+export const genRanHex = (size) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('')
 
 export const CommonHelper = {
   validateEmail,
+  genRanHex,
 }
