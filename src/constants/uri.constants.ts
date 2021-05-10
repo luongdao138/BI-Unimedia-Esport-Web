91321cba52bee04ca980a22ecbd5234aa749d0fd
Tsengel <enkhbat.tsengel@gmail.com>
2021-05-10 11:57:49 +0800

    fix social route

--- src/constants/uri.constants.ts
+++ src/constants/uri.constants.ts
@@ -3,7 +3,7 @@ const DOMAIN = process.env.NEXT_PUBLIC_API
 export const URI = {
   DOMAIN,
   LOGIN: '/v1/auth/login',
-  LOGIN_SOCIAL: '/v1/signup/social',
+  LOGIN_SOCIAL: '/v2/signup/social',
   REGISTER: '/v1/signup/register',
   FORGOT_PASSWORD: '/v1/passwords/forgot',
   FORGOT_CONFIRM: '/v1/passwords/confirm',
