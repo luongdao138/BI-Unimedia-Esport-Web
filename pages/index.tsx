b10b7ef98c5ddeb2bbcb9f9e10172220d7e356cb
bayaroch <bayaroch@gmail.com>
2021-05-10 16:52:36 +0800

    footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,7 @@ import HomeContainer from '@containers/Home'
 import MainLayout from '@layouts/MainLayout'
 
 const HomePage = () => {
-  return (
-    <MainLayout>
-      <HomeContainer />
-    </MainLayout>
-  )
+  return <HomeContainer />
 }
 
 HomePage.Layout = MainLayout
