673ad62ccb85563d44c0956b2bb716186f09e669
Enkhtulga <gantsagaan_enkhtulga@unimedia.co.jp>
2021-05-10 13:54:01 +0800

    some changes

--- src/containers/Home/elements/Header/index.tsx
+++ src/containers/Home/elements/Header/index.tsx
@@ -1,6 +1,8 @@
-import { AppBar, Typography, Toolbar, IconButton, Icon } from '@material-ui/core'
+import { AppBar, Typography, Toolbar } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { useTranslation } from 'react-i18next'
+import ESMenu from '@components/Menu'
+import ESMenuItem from '@components/Menu/MenuItem'
 
 const useStyles = makeStyles(() => ({
   grow: { flexGrow: 1 },
@@ -28,9 +30,15 @@ export const Header: React.FC = () => {
       <AppBar className={classes.appBar} position="static">
         <Toolbar className={classes.toolbar}>
           <Typography variant="h2">{t('common:home.home')}</Typography>
-          <IconButton>
-            <Icon className="fas fa-ellipsis-v" fontSize="small" />
-          </IconButton>
+          <ESMenu>
+            <ESMenuItem
+              onClick={() => {
+                return null
+              }}
+            >
+              コミュニティを作る
+            </ESMenuItem>
+          </ESMenu>
         </Toolbar>
       </AppBar>
     </div>
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -18,6 +18,14 @@ export const Colors = {
   },
   black: '#000000',
   white: '#ffffff',
+  white_opacity: {
+    '30': 'rgba(255,255,255,.3)',
+    '70': 'rgba(255,255,255,.7)',
+  },
+  black_opacity: {
+    '15': 'rgba(0,0,0,.15)',
+    '70': 'rgba(0,0,0,.7)',
+  },
   green: '#2CEBB0',
   avatar: ['#8E47FF', '#476AFF', '#2CEBB0', '#DCEB2C', '#FF9E1A', '#FF6A1C', '#502298', '#2744BF', '#7D8805', '#981643', '#FF4786'],
 }
