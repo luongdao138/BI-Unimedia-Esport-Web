515d2efda0c9412b8447e4eb21a5de7ec9541c56
bayaroch <bayaroch@gmail.com>
2021-05-11 17:39:43 +0800

    search new xd

---
+++ src/containers/ChatSideBar/index.tsx
@@ -0,0 +1,69 @@
+import { Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { IconButton } from '@material-ui/core'
+
+interface ChatSideBarProps {
+  expand: boolean
+  toggleChatBar: (state: boolean) => void
+}
+
+const ChatSideBar: React.FC<ChatSideBarProps> = ({ toggleChatBar, expand }) => {
+  const classes = useStyles(expand)
+  return (
+    <Box className={`${classes.sidebarCont} ${expand ? 'expanded-sidebar' : ''}`}>
+      <IconButton onClick={() => toggleChatBar(!expand)} className={classes.arrowBtn} disableRipple>
+        <span className={classes.arrow}></span>
+      </IconButton>
+      <div className="back-drop"></div>
+    </Box>
+  )
+}
+
+const useStyles = makeStyles({
+  sidebarCont: {
+    width: (expand) => (expand ? 90 : 260),
+    height: 1000,
+    display: 'block',
+    position: 'fixed',
+    transform: (expand) => (expand ? 'translateX(calc(0px))' : 'translateX(calc(-170px))'),
+    transition: 'all 0.1s ease',
+    borderLeft: '1px solid #70707070',
+    top: 61,
+    bottom: 0,
+  },
+  arrow: {
+    display: 'inline-block',
+    position: 'absolute',
+    width: 8,
+    height: 8,
+    background: 'transparent',
+    borderTop: '1px solid #bfbfbf',
+    borderLeft: '1px solid #bfbfbf',
+    transition: 'none',
+    color: 'transparent',
+    transform: (expand) => (expand ? 'rotate(-45deg)' : 'rotate(135deg)'),
+    left: (expand) => (expand ? '7px' : '4px'),
+  },
+  arrowBtn: {
+    background: 'rgba(0,0,0,0.5)',
+    border: '1px solid #ffffff30',
+    borderRight: 'none',
+    borderTopLeftRadius: 4,
+    borderBottomLeftRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 20,
+    height: 50,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    position: 'absolute',
+    left: -20,
+    zIndex: 100,
+    padding: 0,
+    top: 40,
+    '&:hover': {
+      background: 'rgba(0,0,0,0.5)',
+    },
+  },
+})
+export default ChatSideBar
