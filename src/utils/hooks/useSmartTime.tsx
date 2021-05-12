6a5c323a337653350a3e8c8b00292d490686d3a3
bayaroch <bayaroch@gmail.com>
2021-05-12 17:11:59 +0800

    merged removed comment

--- src/utils/hooks/useSmartTime.tsx
+++ src/utils/hooks/useSmartTime.tsx
@@ -5,7 +5,7 @@ const useSmartTime = (data: string | number) => {
   const [time, setTime] = useState<string>('')
   useEffect(() => {
     if (data) {
-      const timestamp = data // Unix time is seconds from epoch. What you're passing is milliseconds from epoch. Divide the value by 1000, before passing it to the method:
+      const timestamp = data
       const currentDate = moment().startOf('day')
       const given = moment(timestamp).format('YYYY-MM-DD')
       const diff = currentDate.diff(given, 'days', false)
