fdc8cdc657ac312c1b2404f77a0e96f4714848fd
boomoo <tumenbat_enkhtsolmon@unimedia.co.jp>
2021-05-12 10:00:05 +0800

    fix

--- src/containers/Followers/index.tsx
+++ src/containers/Followers/index.tsx
@@ -8,11 +8,16 @@ import ESLoader from '@components/Loader'
 import useFollowers from '../../containers/Followers/useFollowers'
 import UserListItem from '@components/UserItem'
 import { useTranslation } from 'react-i18next'
+import DialogContent from '@material-ui/core/DialogContent'
 
 const ESFollowers: React.FC = () => {
   const [open, setOpen] = useState(false)
   const { t } = useTranslation(['common'])
   const { currentUser, users, followers, meta, page } = useFollowers()
+
+  useEffect(() => {
+    followers({ page: 1, user_id: currentUser.id })
+  }, [])
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll)
@@ -21,7 +26,9 @@ const ESFollowers: React.FC = () => {
 
   function handleScroll() {
     if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return
-    if (page && page.current_page !== page.total_pages) followers({ page: page.current_page + 1, user_id: currentUser.id })
+    if (page && page.current_page !== page.total_pages) {
+      //   followers({ page: page.current_page + 1, user_id: currentUser.id })
+    }
   }
 
   const handleClickOpen = () => {
@@ -38,7 +45,7 @@ const ESFollowers: React.FC = () => {
       <label style={{ color: 'white', fontSize: 15, fontWeight: 'bold' }}>{users.length}</label>
       <span>{t('common:followers.th')}</span>
       <ESDialog open={open} handleClose={handleClose}>
-        <Grid container>
+        <DialogContent>
           {users.map((user, i) => (
             <UserListItem data={user} key={i} isFollowed={user.attributes.is_followed} />
           ))}
@@ -49,7 +56,7 @@ const ESFollowers: React.FC = () => {
               </Box>
             </Grid>
           )}
-        </Grid>
+        </DialogContent>
       </ESDialog>
     </div>
   )
