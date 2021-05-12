1d84a75570de299d39a2dc8e8c35c5445fbcd959
“khaynaa” <turmandakh_khayanhyaravaa@unimedia.co.jp>
2021-05-12 08:08:49 +0800

    add side mune item selected mode

---
+++ pages/tournaments/index.tsx
@@ -0,0 +1,9 @@
+import MainLayout from '@layouts/MainLayout'
+
+const TournamentsPage = () => {
+  return <div>Tournaments</div>
+}
+
+TournamentsPage.Layout = MainLayout
+
+export default TournamentsPage
--- src/containers/SideMenu/index.tsx
+++ src/containers/SideMenu/index.tsx
@@ -1,14 +1,15 @@
 import Box from '@material-ui/core/Box'
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
+import MuiListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
 import { Colors } from '@theme/colors'
 import Icon from '@material-ui/core/Icon'
 import ProfileAvatar from '@components/ProfileAvatar'
 import { Typography } from '@material-ui/core'
-
+import { useRouter } from 'next/router'
+import { useTranslation } from 'react-i18next'
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
@@ -66,8 +67,39 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+const ListItem = withStyles({
+  root: {
+    '&$selected': {
+      backgroundColor: 'transparent',
+      color: Colors.primary,
+      '& .MuiListItemIcon-root': {
+        color: Colors.primary,
+      },
+      '& .MuiListItemText-root': {
+        color: Colors.primary,
+      },
+    },
+    '&$selected:hover': {
+      backgroundColor: 'transparent',
+      color: Colors.primary,
+      '& .MuiListItemIcon-root': {
+        color: Colors.primary,
+      },
+      '& .MuiListItemText-root': {
+        color: Colors.primary,
+      },
+    },
+  },
+  selected: {},
+})(MuiListItem)
+
 const SideMenu: React.FC = () => {
+  const { t } = useTranslation(['common'])
   const classes = useStyles()
+  const router = useRouter()
+  const isSelected = (routeName: string): boolean => {
+    return router.pathname === routeName
+  }
   return (
     <>
       <Box className={classes.menu}>
@@ -75,51 +107,63 @@ const SideMenu: React.FC = () => {
           <ProfileAvatar size={80} src="/images/avatar.png" />
           <Box width="100%" textAlign="center">
             <Typography variant="h2" className={classes.name}>
-              eXe LAB事務局
+              {t('common:home:exelab')}
             </Typography>
             <Typography variant="body2" className={classes.usercode}>
-              @exelab_staff
+              {t('common:home:exelab_staff')}
             </Typography>
           </Box>
         </Box>
 
         <List component="nav" aria-labelledby="nested-list-subheader" className={classes.root}>
-          <ListItem className={classes.list} button disableRipple>
+          <ListItem className={classes.list} button disableRipple selected={isSelected('/home')} onClick={() => router.push('/home')}>
             <ListItemIcon className={classes.icon}>
               <Icon fontSize="small" className="fa fa-home" />
             </ListItemIcon>
-            <ListItemText className={classes.listText} primary="アリーナ" />
+            <ListItemText className={classes.listText} primary={t('common:home:home')} />
           </ListItem>
-          <ListItem className={classes.list} button disableRipple>
+          <ListItem
+            className={classes.list}
+            button
+            disableRipple
+            selected={isSelected('/tournaments')}
+            onClick={() => router.push('/tournaments')}
+          >
             <ListItemIcon className={classes.icon}>
               <Icon fontSize="small" className="fa fa-trophy" />
             </ListItemIcon>
-            <ListItemText className={classes.listText} primary="ロビー" />
+            <ListItemText className={classes.listText} primary={t('common:home:tournament')} />
           </ListItem>
           <ListItem className={classes.list} button disableRipple>
             <ListItemIcon className={classes.icon}>
               <Icon fontSize="small" className="fa fa-university" />
             </ListItemIcon>
-            <ListItemText className={classes.listText} primary="ロビー" />
+            <ListItemText className={classes.listText} primary={t('common:home:lobby')} />
           </ListItem>
           <ListItem className={classes.list} button disableRipple>
             <ListItemIcon className={classes.icon}>
               <Icon fontSize="small" className="fa fa-users" />
             </ListItemIcon>
-            <ListItemText className={classes.listText} primary="コミュニティ" />
+            <ListItemText className={classes.listText} primary={t('common:home:community')} />
           </ListItem>
           <Box paddingBottom={4} />
           <ListItem className={classes.list} button disableRipple>
             <ListItemIcon className={classes.icon}>
               <Icon fontSize="small" className="fa fa-play-circle" />
             </ListItemIcon>
-            <ListItemText className={classes.listText} primary="ロビー" />
+            <ListItemText className={classes.listText} primary={t('common:home:video')} />
           </ListItem>
-          <ListItem className={classes.list} button disableRipple>
+          <ListItem
+            className={classes.list}
+            button
+            disableRipple
+            selected={isSelected('/settings')}
+            onClick={() => router.push('/settings')}
+          >
             <ListItemIcon className={classes.icon}>
               <Icon fontSize="small" className="fa fa-cog" />
             </ListItemIcon>
-            <ListItemText className={classes.listText} primary="コミュニティ" />
+            <ListItemText className={classes.listText} primary={t('common:home:settings')} />
           </ListItem>
         </List>
       </Box>
