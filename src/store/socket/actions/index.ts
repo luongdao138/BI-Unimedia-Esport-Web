06d9a5c85f9bd17201bcf4bdbfe612112e0862cc
bayaroch <bayaroch@gmail.com>
2021-05-14 17:11:14 +0800

    type

--- src/store/socket/actions/index.ts
+++ src/store/socket/actions/index.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { WEBSOCKET_PREFIX } from '@constants/socket.constants'
 import { AppDispatch } from '@store/store'
 
 export const socketActions = {
-  socketSend: (payload: any) => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  socketSend: (payload: object) => {
     return (dispatch: AppDispatch) => {
       dispatch(socketCreators.socketSend(payload))
     }
@@ -10,7 +12,8 @@ export const socketActions = {
 }
 
 export const socketCreators = {
-  socketSend: (payload: any) => ({
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  socketSend: (payload: object) => ({
     type: `${WEBSOCKET_PREFIX}:SEND`,
     payload: payload,
   }),
