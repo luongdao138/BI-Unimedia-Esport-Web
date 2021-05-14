f42c099ac1723c19564aefdcbf14d7acf23798a0
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-05-14 12:24:42 +0800

    data fetch

--- src/store/notification/selectors/index.ts
+++ src/store/notification/selectors/index.ts
@@ -4,4 +4,4 @@ import { RootState } from '@store/store'
 const getRoot = (state: RootState) => state.notification
 
 export const getNotificationList = createSelector(getRoot, (state) => state.my_notification_list)
-export const getNotificationListMeta = createSelector(getRoot, (state) => state.searchUsersMeta)
+export const getNotificationListMeta = createSelector(getRoot, (state) => state.my_notification_list_meta)
