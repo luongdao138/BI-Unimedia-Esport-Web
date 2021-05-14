0cf2e9567afd696b298e137d8bc2010052928840
dsukhochir <davaajav_sukhochir@unimedia.co.jp>
2021-05-14 12:23:34 +0800

    fix

--- src/containers/Notifications/index.tsx
+++ src/containers/Notifications/index.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react'
+import useNotificationList from './useNotificationList'
 const NotificationContainer: React.FC = () => {
-  return <>sdf</>
+  const { notifications, fetchNotifications } = useNotificationList()
+  useEffect(() => {
+    fetchNotifications({ page: 1 })
+  }, [])
+  return (
+    <>
+      {notifications.map((notification, i) => (
+        <span key={i}>{notification.attributes.nickname}</span>
+      ))}
+    </>
+  )
 }
 export default NotificationContainer
