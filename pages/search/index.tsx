0600e6edadf6f52c8b108f5224cfdc3e8ae0a8bb
bilguun_b <batnasan_bilguun@unimedia.co.jp>
2021-05-17 09:57:30 +0800

    #15 additional

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
 import UserSearchContainer from '@containers/Search/UserSearch'
 import TournamentSearchContainer from '@containers/Search/TournamentSearch'
+import { Grid, Box, makeStyles, Typography, IconButton, Icon } from '@material-ui/core'
 import MainLayout from '@layouts/MainLayout'
 import { searchTypes } from '@constants/common.constants'
+import { useTranslation } from 'react-i18next'
 import { useRouter } from 'next/router'
+import { Colors } from '@theme/colors'
 import _ from 'lodash'
 import PageWithLayoutType from '@constants/page'
 
 const SearchPage: PageWithLayoutType = () => {
+  const { t } = useTranslation(['common'])
+  const classes = useStyles()
   const router = useRouter()
   const [type, setType] = useState<number>()
 
@@ -28,8 +33,33 @@ const SearchPage: PageWithLayoutType = () => {
     }
   }
 
-  return renderSwitch()
+  return (
+    <Grid container className={classes.container}>
+      <Box pb={3} display="flex" flexDirection="row" alignItems="center">
+        <IconButton className={classes.iconButtonBg}>
+          <Icon className="fa fa-arrow-left" fontSize="small" />
+        </IconButton>
+        <Typography variant="h2" noWrap>
+          {t('common:user.user_results')}
+        </Typography>
+      </Box>
+      {renderSwitch()}
+    </Grid>
+  )
 }
+
+const useStyles = makeStyles(() => ({
+  iconButtonBg: {
+    backgroundColor: `${Colors.grey[1000]}80`,
+    '&:focus': {
+      backgroundColor: `${Colors.grey[1000]}80`,
+    },
+  },
+  container: {
+    padding: 24,
+    paddingTop: 16,
+  },
+}))
 
 SearchPage.Layout = MainLayout
 
--- src/containers/Search/UserSearch.tsx
+++ src/containers/Search/UserSearch.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import { Grid, Box, makeStyles, Typography, IconButton, Icon } from '@material-ui/core'
+import { Grid, Box, Typography, makeStyles } from '@material-ui/core'
 import useUserSearch from './useUserSearch'
 import ESLoader from '@components/Loader'
 import UserListItem from '@components/UserItem'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { useTranslation } from 'react-i18next'
-import { Colors } from '@theme/colors'
 import { useRouter } from 'next/router'
 import _ from 'lodash'
 
@@ -12,7 +12,7 @@ const UserSearchContainer: React.FC = () => {
   const { t } = useTranslation(['common'])
   const classes = useStyles()
   const router = useRouter()
-  const { searchUsers, userSearch, meta, page } = useUserSearch()
+  const { searchUsers, userSearch, page } = useUserSearch()
   const [keyword, setKeyword] = useState<string>('')
 
   useEffect(() => {
@@ -23,31 +23,14 @@ const UserSearchContainer: React.FC = () => {
     }
   }, [router.query])
 
-  useEffect(() => {
-    if (document.documentElement.scrollHeight > document.documentElement.clientHeight) return
-    if (page && page.current_page !== page.total_pages) userSearch({ page: page.current_page + 1, keyword: keyword })
-  }, [searchUsers])
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [page])
-
-  const handleScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return
-    if (page && page.current_page !== page.total_pages) userSearch({ page: page.current_page + 1, keyword: '' })
+  const loadMore = () => {
+    if (page && page.current_page !== page.total_pages) {
+      userSearch({ page: page.current_page + 1, keyword: keyword })
+    }
   }
 
   return (
-    <Grid container className={classes.container}>
-      <Box pb={3} display="flex" flexDirection="row" alignItems="center">
-        <IconButton className={classes.iconButtonBg}>
-          <Icon className="fa fa-arrow-left" fontSize="small" />
-        </IconButton>
-        <Typography variant="h2" noWrap>
-          {t('common:user.user_results')}
-        </Typography>
-      </Box>
+    <Grid container>
       {!!page && (
         <Grid item xs={12}>
           <Typography variant="caption" gutterBottom>
@@ -55,30 +38,31 @@ const UserSearchContainer: React.FC = () => {
           </Typography>
         </Grid>
       )}
-      {searchUsers.map((user, i) => (
-        <UserListItem data={user} key={i} isFollowed={user.attributes.is_followed} />
-      ))}
-      {meta.pending && (
-        <Grid item xs={12}>
-          <Box my={4} display="flex" justifyContent="center" alignItems="center">
-            <ESLoader />
-          </Box>
-        </Grid>
-      )}
+      <InfiniteScroll
+        className={classes.container}
+        dataLength={searchUsers.length}
+        next={loadMore}
+        hasMore={page && page.current_page !== page.total_pages}
+        loader={
+          <Grid item xs={12}>
+            <Box my={4} display="flex" justifyContent="center" alignItems="center">
+              <ESLoader />
+            </Box>
+          </Grid>
+        }
+      >
+        {searchUsers.map((user, i) => (
+          <UserListItem data={user} key={i} isFollowed={user.attributes.is_followed} />
+        ))}
+      </InfiniteScroll>
     </Grid>
   )
 }
 
 const useStyles = makeStyles(() => ({
-  iconButtonBg: {
-    backgroundColor: `${Colors.grey[1000]}80`,
-    '&:focus': {
-      backgroundColor: `${Colors.grey[1000]}80`,
-    },
-  },
   container: {
-    padding: 24,
-    paddingTop: 16,
+    display: 'flex',
+    flexWrap: 'wrap',
   },
 }))
 
