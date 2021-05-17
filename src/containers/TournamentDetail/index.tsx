b7402afe71ddedf0709beb7b64db218e9f469594
Tsengel <enkhbat.tsengel@gmail.com>
2021-05-17 17:57:10 +0800

    add detail skeleton

--- src/containers/TournamentDetail/index.tsx
+++ src/containers/TournamentDetail/index.tsx
@@ -1,13 +1,26 @@
+import { ReactNode } from 'react'
 import useTournamentDetail from './useTournamentDetail'
 
 import TournamentDetailHeader from '@components/TournamentDetailHeader'
 import { useRouter } from 'next/router'
+import { TournamentStatus } from '@services/tournament.service'
+
+import RecruitingAction from './RecruitingAction'
 
 const TournamentDetail: React.FC = () => {
   const router = useRouter()
   const { tournament, meta } = useTournamentDetail()
-
   const handleBack = () => router.back()
+
+  const actionComponent: Record<TournamentStatus, ReactNode> = {
+    in_progress: <RecruitingAction tournament={tournament} />,
+    cancelled: <></>,
+    completed: <></>,
+    ready: <></>,
+    ready_to_start: <></>,
+    recruiting: <></>,
+    recruitment_closed: <></>,
+  }
   return (
     <div>
       {meta.pending && !tournament ? (
@@ -19,9 +32,10 @@ const TournamentDetail: React.FC = () => {
             cover={tournament?.attributes?.cover_image}
             onHandleBack={handleBack}
           >
+            {actionComponent[tournament.attributes.status]}
             <pre>{JSON.stringify(tournament, null, 2)}</pre>
           </TournamentDetailHeader>
-          {/* <pre>{JSON.stringify(tournament, null, 2)}</pre> */}
+          <pre>{JSON.stringify(tournament, null, 2)}</pre>
         </>
       )}
     </div>
