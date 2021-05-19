9e2870da0d9d41061ef79fcb2b617ebc6a1f7120
boomoo <tumenbat_enkhtsolmon@unimedia.co.jp>
2021-05-19 15:15:54 +0800

    common comps added.

--- pages/settings/user_security_settings/index.tsx
+++ pages/settings/user_security_settings/index.tsx
@@ -1,8 +1,15 @@
 import MainLayout from '@layouts/MainLayout'
 import PageWithLayoutType from '@constants/page'
+import SettingsRowItem from '@components/SettingsRowItem'
 
 const UserSecuritySettingsPage: PageWithLayoutType = () => {
-  return <div></div>
+  return (
+    <div>
+      <SettingsRowItem title="大会履歴" />
+      <SettingsRowItem title="アクティビティ" />
+      <SettingsRowItem title="プロフィール" />
+    </div>
+  )
 }
 
 UserSecuritySettingsPage.Layout = MainLayout
