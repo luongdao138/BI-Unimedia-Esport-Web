18667f2fce43fe3bb08526c22ebdb5d9e669a759
boomoo <tumenbat_enkhtsolmon@unimedia.co.jp>
2021-05-19 13:47:45 +0800

    initial commit

---
+++ pages/settings/user_security_settings/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '@layouts/MainLayout'
+import PageWithLayoutType from '@constants/page'
+
+const UserSecuritySettingsPage: PageWithLayoutType = () => {
+  return <div></div>
+}
+
+UserSecuritySettingsPage.Layout = MainLayout
+
+export default UserSecuritySettingsPage
