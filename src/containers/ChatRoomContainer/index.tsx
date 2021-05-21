acf6515f70d738b197d879b5e3742538b5b5ae72
bayaroch <bayaroch@gmail.com>
2021-05-21 16:33:35 +0800

    uploader

--- src/containers/ChatRoomContainer/index.tsx
+++ src/containers/ChatRoomContainer/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { makeStyles, Box, Typography } from '@material-ui/core'
 import MessageInputArea from '@components/Chat/MessageInputArea'
 import { socketActions } from '@store/socket/actions'
 import { useAppDispatch, useAppSelector } from '@store/hooks'
+import ImageUploader from './ImageUploader'
 import { currentUserId } from '@store/auth/selectors'
 import { messages } from '@store/socket/selectors'
 import { ChatSuggestionList } from '@components/Chat/types/chat.types'
@@ -11,9 +12,7 @@ import { v4 as uuidv4 } from 'uuid'
 import _ from 'lodash'
 import moment from 'moment'
 import Loader from '@components/Loader'
-import { useDropzone } from 'react-dropzone'
-import { getPreSignedUrl, upload } from '@services/image.service'
-import { UPLOADER_TYPE, ACTION_TYPE } from '@constants/image.constants'
+import { ACTIONS } from '@components/Chat/constants'
 
 interface ChatRoomContainerProps {
   roomId: string | string[]
@@ -82,18 +81,19 @@ const users: ChatSuggestionList[] = [
   },
 ]
 
+export interface UploadStateType {
+  id: string | null
+  uploading: boolean
+}
+
 const ChatRoomContainer: React.FC<ChatRoomContainerProps> = ({ roomId }) => {
-  const dropZone = useDropzone({
-    noClick: true,
-    noKeyboard: true,
-    multiple: false,
-    accept: 'image/*',
-    onDrop: (files) => handleFile(files),
-  })
+  const [uploadMeta, setMeta] = useState<UploadStateType>({ id: null, uploading: false })
 
   const classes = useStyles()
 
   const dispatch = useAppDispatch()
+
+  const ref = useRef<{ handleUpload: () => void }>(null)
 
   const userId = useAppSelector(currentUserId)
   const data = useAppSelector(messages)
@@ -110,49 +110,6 @@ const ChatRoomContainer: React.FC<ChatRoomContainerProps> = ({ roomId }) => {
     }
   }, [userId, roomId])
 
-  const handleFile = (files: File[]) => {
-    const file = files[0]
-    const reader = new FileReader()
-    if (file) {
-      // eslint-disable-next-line no-console
-      imageProcess(file).then((result) => {
-        result
-      })
-      reader.readAsDataURL(file)
-    }
-  }
-
-  const imageProcess = async (file: File) => {
-    const params = {
-      type: UPLOADER_TYPE.CHAT,
-      room: roomId as string,
-      fileName: file.name,
-      contentType: file.type,
-      action_type: ACTION_TYPE.CREATE,
-    }
-
-    const res = await getPreSignedUrl(params)
-    const fileUrl = res.file_url
-    const signedUrl = res.url
-    await upload(file, signedUrl)
-
-    const currentTimestamp = moment().valueOf()
-    const clientId = uuidv4()
-
-    const payload = {
-      action: CHAT_ACTION_TYPE.SEND_MESSAGE,
-      roomId: roomId,
-      createdAt: currentTimestamp,
-      userId: userId,
-      msg: URL.createObjectURL(file),
-      clientId: clientId,
-      type: CHAT_MESSAGE_TYPE.IMAGE,
-    }
-    dispatch(socketActions.messagePending(payload))
-    payload.msg = fileUrl
-    dispatch(socketActions.socketSend(payload))
-  }
-
   const handlePress = (text: string) => {
     const currentTimestamp = moment().valueOf()
     const clientId = uuidv4()
@@ -167,9 +124,15 @@ const ChatRoomContainer: React.FC<ChatRoomContainerProps> = ({ roomId }) => {
     }
     dispatch(socketActions.sendMessage(payload))
   }
-  const handlePressActionButton = (_type: number) => {
-    dropZone.open()
+  const handlePressActionButton = (type: number) => {
+    if (type === ACTIONS.IMAGE_UPLOAD)
+      if (ref.current && !uploadMeta.uploading) {
+        const uploaderClientId = uuidv4()
+        setMeta({ id: uploaderClientId, uploading: false })
+        ref.current.handleUpload()
+      }
   }
+
   const renderLoader = () => {
     if (data === undefined) {
       return (
@@ -179,6 +142,26 @@ const ChatRoomContainer: React.FC<ChatRoomContainerProps> = ({ roomId }) => {
       )
     }
     return null
+  }
+
+  const imageEventHandler = (url: string, isPending: boolean) => {
+    const currentTimestamp = moment().valueOf()
+    const payload = {
+      action: CHAT_ACTION_TYPE.SEND_MESSAGE,
+      roomId: roomId,
+      createdAt: currentTimestamp,
+      userId: userId,
+      msg: url,
+      clientId: uploadMeta.id,
+      type: CHAT_MESSAGE_TYPE.IMAGE,
+    }
+    if (isPending) {
+      setMeta({ ...uploadMeta, uploading: true })
+      dispatch(socketActions.messagePending(payload))
+    } else {
+      dispatch(socketActions.socketSend(payload))
+      setMeta({ id: null, uploading: false })
+    }
   }
 
   return (
@@ -206,9 +189,7 @@ const ChatRoomContainer: React.FC<ChatRoomContainerProps> = ({ roomId }) => {
       <Box className={classes.input}>
         <MessageInputArea onPressSend={handlePress} users={users} onPressActionButton={handlePressActionButton} />
       </Box>
-      <div {...dropZone.getRootProps({ className: classes.dropZone })}>
-        <input {...dropZone.getInputProps()} />
-      </div>
+      <ImageUploader ref={ref} roomId={roomId} onResponse={imageEventHandler} onImageSelected={imageEventHandler} />
     </Box>
   )
 }
