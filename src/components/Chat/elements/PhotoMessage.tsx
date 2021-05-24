46b2b24e102978678dc2204b46d22a4672349359
bayaroch <bayaroch@gmail.com>
2021-05-24 19:05:36 +0800

    ungoing implement react virtulizied

--- src/components/Chat/elements/PhotoMessage.tsx
+++ src/components/Chat/elements/PhotoMessage.tsx
@@ -10,13 +10,26 @@ const PhotoMessage: React.FC<MessageImageProps> = (props) => {
 
   const { currentMessage } = props
 
-  return <Box className={classes.box}>{<img src={currentMessage} />}</Box>
+  return <Box className={classes.box}>{<img className={classes.img} src={currentMessage} />}</Box>
 }
 
 const useStyles = makeStyles(() => ({
   box: {
     width: '100%',
     height: '100%',
+    paddingTop: '66.6666%',
+    position: 'relative',
+    display: 'block',
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    objectFit: 'cover',
   },
 }))
 
