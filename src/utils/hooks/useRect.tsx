fd71aa804beada6e3c907b645c674559412603d4
bayaroch <bayaroch@gmail.com>
2021-05-24 19:18:20 +0800

    type add

--- src/utils/hooks/useRect.tsx
+++ src/utils/hooks/useRect.tsx
@@ -1,5 +1,4 @@
-// source: https://gist.github.com/morajabi/523d7a642d8c0a2f71fcfa0d8b3d2846
-import { useLayoutEffect, useCallback, useState } from 'react'
+import React, { useLayoutEffect, useCallback, useState } from 'react'
 
 type RectResult = {
   bottom: number
@@ -10,46 +9,62 @@ type RectResult = {
   width: number
 }
 
-function getRect<T extends HTMLElement>(element?: T): RectResult {
-  let rect: RectResult = {
-    bottom: 0,
-    height: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-    width: 0,
-  }
-  if (element) rect = element.getBoundingClientRect()
-  return rect
-}
-
-export function useRect<T extends HTMLElement>(ref: React.RefObject<T>): RectResult {
-  const [rect, setRect] = useState<RectResult>(ref && ref.current ? getRect(ref.current) : getRect())
+export const useRect = (ref: React.RefObject<any>): RectResult => {
+  const [rect, setRect] = useState(getRect(ref ? ref.current : null))
 
   const handleResize = useCallback(() => {
-    if (!ref.current) return
-    setRect(getRect(ref.current)) // Update client rect
+    if (!ref.current) {
+      return
+    }
+
+    // Update client rect
+    setRect(getRect(ref.current))
   }, [ref])
 
   useLayoutEffect(() => {
     const element = ref.current
-    if (!element) return
+    if (!element) {
+      return
+    }
 
     handleResize()
 
     if (typeof ResizeObserver === 'function') {
       let resizeObserver = new ResizeObserver(() => handleResize())
       resizeObserver.observe(element)
+
       return () => {
-        if (!resizeObserver) return
+        if (!resizeObserver) {
+          return
+        }
+
         resizeObserver.disconnect()
         resizeObserver = null
       }
     } else {
-      window.addEventListener('resize', handleResize) // Browser support, remove freely
-      return () => window.removeEventListener('resize', handleResize)
+      // Browser support, remove freely
+      window.addEventListener('resize', handleResize)
+
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
     }
   }, [ref.current])
 
   return rect
+}
+
+function getRect(element) {
+  if (!element) {
+    return {
+      bottom: 0,
+      height: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 0,
+    }
+  }
+
+  return element.getBoundingClientRect()
 }
