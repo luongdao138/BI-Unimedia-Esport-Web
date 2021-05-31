a2707133d474d0e18139fba9e11ceabfc5298c14
Nasanjargal <nasaa1997@gmail.com>
2021-05-31 22:49:50 +0800

    fix

--- pages/settings/account/change-password/index.tsx
+++ pages/settings/account/change-password/index.tsx
@@ -1,10 +1,8 @@
 import AccountSettingsChangePasswordContainer from '@containers/Settings/Account/ChangePassword'
 import BlankLayout from '@layouts/BlankLayout'
 import PageWithLayoutType from '@constants/page'
-import useAuthenticated from './../useAuthenticated'
 
 const AccountChangePasswordPage: PageWithLayoutType = () => {
-  useAuthenticated()
   return <AccountSettingsChangePasswordContainer />
 }
 
