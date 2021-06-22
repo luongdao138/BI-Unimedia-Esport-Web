c6186b8bb3f9f949ab6f97c3215b29038d525968
batjargal <dugardagva_batjargal@unimedia.co.jp>
2021-06-22 17:26:31 +0800

    report screen and activity log fixed

--- src/containers/Report/index.tsx
+++ src/containers/Report/index.tsx
@@ -1,16 +1,14 @@
 import { useEffect } from 'react'
-import { Button, Box, Typography, Grid } from '@material-ui/core'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogActions from '@material-ui/core/DialogActions'
-import Radio from '@material-ui/core/Radio'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { Box, Typography, Grid, FormControlLabel, DialogContent, DialogActions, Radio } from '@material-ui/core'
 import Input from '@components/Input'
 import RadioVertical from '@components/RadioVertical'
 import ESLoader from '@components/Loader'
 import ESDialog from '@components/Dialog'
 import ProfileAvatar from '@components/ProfileAvatar'
+import ButtonPrimary from '@components/ButtonPrimary'
 import { ReportParams } from '@services/report.service'
 import { useFormik } from 'formik'
+import { useStore } from 'react-redux'
 import useReport from './useReport'
 import useReasons from './useReasons'
 import { CommonHelper } from '@utils/helpers/CommonHelper'
@@ -37,9 +35,11 @@ export interface ESReportProps {
 
 const ESReport: React.FC<ESReportProps> = ({ data, target_id, room_id, chat_id, reportType, msg_body, open, handleClose, members }) => {
   const classes = useStyles()
-  const { createReport, meta } = useReport()
+  const store = useStore()
+  const { createReport, meta, userEmail } = useReport()
   const { reasons, fetchReasons } = useReasons()
   const { t } = useTranslation('common')
+  const attr = data?.attributes
 
   useEffect(() => {
     fetchReasons({ page: 1 })
@@ -50,8 +50,16 @@ const ESReport: React.FC<ESReportProps> = ({ data, target_id, room_id, chat_id, 
       .test('email-validation', t('common.error'), (value) => {
         return CommonHelper.validateEmail(value)
       })
-      .required(t('common.required')),
-    description: Yup.string().required(t('common.required')).max(1000, t('common.too_long')),
+      .required(t('common.required'))
+      .test('nickname', t('common.contains_ngword'), function (value) {
+        return CommonHelper.matchNgWords(store, value).length <= 0
+      }),
+    description: Yup.string()
+      .required(t('common.required'))
+      .max(1000, t('common.too_long'))
+      .test('nickname', t('common.contains_ngword'), function (value) {
+        return CommonHelper.matchNgWords(store, value).length <= 0
+      }),
     reason_id: Yup.number()
       .test('reason_id', '', (value) => {
         return value !== -1
@@ -64,7 +72,7 @@ const ESReport: React.FC<ESReportProps> = ({ data, target_id, room_id, chat_id, 
       description: '',
       reason_id: reasons[0] ? Number(reasons[0].id) : 1,
       report_type: 0,
-      user_email: '',
+      user_email: userEmail ?? '',
     },
     enableReinitialize: true,
     validationSchema,
@@ -98,98 +106,97 @@ const ESReport: React.FC<ESReportProps> = ({ data, target_id, room_id, chat_id, 
   }, [meta.loaded])
 
   return (
-    <div>
-      <ESDialog
-        title={t('user_report.title')}
-        open={open}
-        handleClose={() => {
-          handleClose()
-          formik.resetForm()
-        }}
-      >
-        <form onSubmit={formik.handleSubmit}>
-          <DialogContent className={classes.dialogContent}>
-            {data && (reportType == REPORT_TYPE.USER_LIST || reportType == REPORT_TYPE.CHAT) ? (
-              <Grid container spacing={2}>
-                <Grid item sm={2} xs={12}>
-                  <ProfileAvatar src={data.attributes.avatar_url} editable={false} />
-                </Grid>
-                <Grid item sm={10} xs={12}>
-                  <Box mt={4}>
-                    <Typography className={classes.nickname}>{data.attributes.nickname}</Typography>
-                    <Typography className={classes.userCode}>{data.attributes.user_code}</Typography>
-                    {msg_body && CommonHelper.isMediaURL(msg_body) ? (
-                      <Box className={classes.imgBox} height={100} width={100}>
-                        <img src={msg_body} className={classes.img} />
-                      </Box>
-                    ) : (
-                      <TextMessage members={members} text={msg_body} color={Colors.white} />
-                    )}
-                  </Box>
-                </Grid>
+    <ESDialog
+      title={t('user_report.title')}
+      open={open}
+      handleClose={() => {
+        handleClose()
+        formik.resetForm()
+      }}
+      className={'scroll-bar'}
+    >
+      <form onSubmit={formik.handleSubmit}>
+        <DialogContent className={classes.dialogContent}>
+          {attr && (reportType == REPORT_TYPE.USER_LIST || reportType == REPORT_TYPE.CHAT) ? (
+            <Grid container spacing={2}>
+              <Grid item sm={2} xs={12}>
+                <ProfileAvatar src={attr?.avatar_url} editable={false} alt={attr?.nickname} />
               </Grid>
-            ) : null}
+              <Grid item sm={10} xs={12}>
+                <Box mt={4}>
+                  <Typography className={classes.nickname}>{attr?.nickname}</Typography>
+                  <Typography className={classes.userCode}>{attr?.user_code}</Typography>
+                  {msg_body && CommonHelper.isMediaURL(msg_body) ? (
+                    <Box className={classes.imgBox} height={100} width={100}>
+                      <img src={msg_body} className={classes.img} />
+                    </Box>
+                  ) : (
+                    <TextMessage members={members} text={msg_body} color={Colors.white} />
+                  )}
+                </Box>
+              </Grid>
+            </Grid>
+          ) : null}
 
-            <Box mt={2}>
-              <Typography>{t('user_report.desc_first')}</Typography>
-              <Typography>{t('user_report.desc_second')}</Typography>
-              <Typography>{t('user_report.desc_third')}</Typography>
-            </Box>
+          <Box mt={2}>
+            <Typography>{t('user_report.desc_first')}</Typography>
+            <Typography>{t('user_report.desc_second')}</Typography>
+            <Typography>{t('user_report.desc_third')}</Typography>
+          </Box>
 
-            <Box mt={1}></Box>
+          <Box mt={1}></Box>
 
-            <RadioVertical
-              id="reason_id"
-              name="reason_id"
-              value={formik.values.reason_id}
-              onChange={formik.handleChange}
-              required
-              label={t('user_report.reason')}
-              helperText={formik.touched.reason_id && formik.errors.reason_id}
-            >
-              {reasons.map((g, idx) => (
-                <FormControlLabel key={idx} value={Number(g.id)} control={<Radio color="primary" />} label={g.attributes.reason} />
-              ))}
-            </RadioVertical>
+          <RadioVertical
+            id="reason_id"
+            name="reason_id"
+            value={formik.values.reason_id + ''}
+            onChange={formik.handleChange}
+            required
+            label={t('user_report.reason')}
+            helperText={formik.touched.reason_id && formik.errors.reason_id}
+          >
+            {reasons.map((g, idx) => (
+              <FormControlLabel key={idx} value={g.id} control={<Radio color="primary" />} label={g.attributes.reason} />
+            ))}
+          </RadioVertical>
 
-            <Box mt={1}></Box>
-            <Input
-              id="description"
-              name="description"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-              labelPrimary={t('user_report.reason_desc')}
-              placeholder={t('user_report.reason_desc')}
-              fullWidth
-              required
-              helperText={formik.touched.description && formik.errors.description}
-              error={formik.touched.description && !!formik.errors.description}
-              multiline
-              rows={4}
-            />
-            <Box mt={1}></Box>
-            <Input
-              id="user_email"
-              name="user_email"
-              value={formik.values.user_email}
-              onChange={formik.handleChange}
-              labelPrimary={t('user_report.reporter_email')}
-              fullWidth
-              required
-              helperText={formik.touched.user_email && formik.errors.user_email}
-              error={formik.touched.user_email && !!formik.errors.user_email}
-            />
-            <Box mt={1}></Box>
-          </DialogContent>
-          <DialogActions style={{ justifyContent: 'center' }}>
-            <Button color="primary" type="submit" disabled={meta.pending}>
-              {t('user_report.btn_text')}
-            </Button>
-            {meta.pending ? <ESLoader /> : null}
-          </DialogActions>
-        </form>
-      </ESDialog>
-    </div>
+          <Box mt={1}></Box>
+          <Input
+            id="description"
+            name="description"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            labelPrimary={t('user_report.reason_desc')}
+            placeholder={t('user_report.reason_desc')}
+            fullWidth
+            required
+            helperText={formik.errors.description}
+            error={!!formik.errors.description}
+            multiline
+            rows={4}
+          />
+          <Box mt={1}></Box>
+          <Input
+            id="user_email"
+            name="user_email"
+            value={formik.values.user_email}
+            onChange={formik.handleChange}
+            labelPrimary={t('user_report.reporter_email')}
+            fullWidth
+            required
+            helperText={formik.errors.user_email}
+            error={!!formik.errors.user_email}
+          />
+          <Box mt={1}></Box>
+        </DialogContent>
+        <DialogActions style={{ justifyContent: 'center' }}>
+          <ButtonPrimary type="submit" round disabled={(typeof formik.errors != undefined && !_.isEmpty(formik.errors)) || meta.pending}>
+            {t('user_report.btn_text')}
+          </ButtonPrimary>
+          {meta.pending ? <ESLoader /> : null}
+        </DialogActions>
+      </form>
+    </ESDialog>
   )
 }
 
@@ -214,10 +221,20 @@ const useStyles = makeStyles(() => ({
     margin: 10,
   },
   dialogContent: {
+    overflow: 'hidden',
     scrollbarWidth: 'none' /* Firefox */,
-    '&::-webkit-scrollbar': {
+    '&::-webkit-scrollbar-track': {
+      '&::-webkit-box-shadow': 'none !important',
+      backgroundColor: 'transparent',
       width: 0,
       height: 0,
+    },
+    '&::-webkit-scrollbar': {
+      backgroundColor: 'transparent',
+      width: '3px !important',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'transparent',
     },
   },
 }))
