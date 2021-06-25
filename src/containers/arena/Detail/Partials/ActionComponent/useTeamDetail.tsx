0df1b1b20e036283dbbc9af80ee0b1c1948285d5
bayr-magnai <baldangombo_bayarmagnai@unimedia.co.jp>
2021-06-25 18:05:42 +0800

    comments removed

--- src/containers/arena/Detail/Partials/ActionComponent/useTeamDetail.tsx
+++ src/containers/arena/Detail/Partials/ActionComponent/useTeamDetail.tsx
@@ -5,20 +5,16 @@ import store from '@store/arena'
 
 const { selectors, actions } = store
 const _getMeta = createMetaSelector(actions.getTournamentTeamDetail)
-// const _changeMeta = createMetaSelector(actions.changeParticipantName)
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const useTeamDetail = () => {
   const dispatch = useAppDispatch()
   const teamDetail = useAppSelector(selectors.getTeamDetail)
   const getMeta = useAppSelector(_getMeta)
-  //   const changeMeta = useAppSelector(_changeMeta)
 
   const getTeamDetail = (teamId: number) => dispatch(actions.getTournamentTeamDetail(teamId))
-  //   const changeName = (hashKey: string, name: string) => dispatch(actions.changeParticipantName({ hash_key: hashKey, data: { name: name } }))
   const resetMeta = () => dispatch(clearMetaData(actions.getParticipantName.typePrefix))
   const isPending = getMeta.pending
-  //   const changeDone = changeMeta.loaded || changeMeta.error
 
   return { getTeamDetail, resetMeta, isPending, teamDetail }
 }
