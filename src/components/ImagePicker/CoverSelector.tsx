796483745fb61e5b4f5eb1791fb0e7a78be2d6c8
batjargal <dugardagva_batjargal@unimedia.co.jp>
2021-06-27 22:29:17 +0800

    try fix 14

--- src/components/ImagePicker/CoverSelector.tsx
+++ src/components/ImagePicker/CoverSelector.tsx
@@ -1,13 +1,13 @@
 import { useState, useCallback, useEffect } from 'react'
 import { Box, Typography, Slider, Link, Theme } from '@material-ui/core'
 import getCroppedImg from './Partials/cropImage'
-import { calculateDimensionsCover } from './Partials/calculateDimensions'
+// import { calculateDimensionsCover } from './Partials/calculateDimensions'
 import ESDialog from '@components/Dialog'
 import ButtonPrimary from '@components/ButtonPrimary'
-import Image from 'next/image'
-import { useDropzone } from 'react-dropzone'
-import Cropper from 'react-easy-crop'
-import { CameraAlt as Camera, Crop169 as RectIcon } from '@material-ui/icons'
+// import Image from 'next/image'
+// import { useDropzone } from 'react-dropzone'
+// import Cropper from 'react-easy-crop'
+import { Crop169 as RectIcon } from '@material-ui/icons'
 import ESLoader from '@components/Loader'
 import i18n from '@locales/i18n'
 // import { Colors } from '@theme/colors'
@@ -50,15 +50,15 @@ const ImageSlider = withStyles({
 const STATIC_WIDTH = 600
 const STATIC_HEIGHT = 200
 
-const CoverSelector: React.FC<CoverSelectorProps> = ({ src, cancel, onUpdate }) => {
+const CoverSelector: React.FC<CoverSelectorProps> = ({ cancel, onUpdate }) => {
   const [rawFile, setRawFile] = useState<null | File>(null)
   const [file, setFile] = useState<any>(null)
-  const [crop, setCrop] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+  // const [crop, setCrop] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
   const [zoom, setZoom] = useState<number>(1)
   const [uploading, setUploading] = useState<boolean>(false)
-  const [fitType, setFit] = useState<'contain' | 'vertical-cover' | 'horizontal-cover'>('horizontal-cover')
-  const [mediaDimensions, setMediaDimensions] = useState<{ width: number; height: number }>({ width: STATIC_WIDTH, height: STATIC_HEIGHT })
+  // const [fitType, setFit] = useState<'contain' | 'vertical-cover' | 'horizontal-cover'>('horizontal-cover')
+  // const [mediaDimensions, setMediaDimensions] = useState<{ width: number; height: number }>({ width: STATIC_WIDTH, height: STATIC_HEIGHT })
   const { width: containerWidth } = useWindowDimensions(64)
   const [dynamicWidth, setDynamicWidth] = useState<number>(STATIC_WIDTH)
   const classes = useStyles({ width: dynamicWidth })
@@ -74,35 +74,35 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ src, cancel, onUpdate }) 
     setDynamicWidth(containerWidth > STATIC_WIDTH ? STATIC_WIDTH : containerWidth)
   }, [containerWidth])
 
-  const dropZoneConfig = {
-    accept: 'image/*',
-    onDrop: (files: any) => handleChange(files),
-  }
-  const { getRootProps, getInputProps } = useDropzone(dropZoneConfig)
+  // const dropZoneConfig = {
+  //   accept: 'image/*',
+  //   onDrop: (files: any) => handleChange(files),
+  // }
+  // const { getRootProps, getInputProps } = useDropzone(dropZoneConfig)
 
-  const handleChange = (files: Array<File>) => {
-    const f = files[0]
-    const reader = new FileReader()
-    if (f) {
-      setRawFile(f)
-      reader.onload = (e) => {
-        const img = document.createElement('img')
-        img.src = e.target.result as string
-        img.onload = function () {
-          const w = img.naturalWidth || img.width
-          const h = img.naturalHeight || img.height
-          setMediaDimensions(calculateDimensionsCover(w, h, dynamicWidth, STATIC_HEIGHT))
-          if (h >= w) {
-            setFit('horizontal-cover')
-          } else if (w > h) {
-            setFit('vertical-cover')
-          }
-        }
-        setFile(reader.result)
-      }
-      reader.readAsDataURL(f)
-    }
-  }
+  // const handleChange = (files: Array<File>) => {
+  //   const f = files[0]
+  //   const reader = new FileReader()
+  //   if (f) {
+  //     setRawFile(f)
+  //     reader.onload = (e) => {
+  //       const img = document.createElement('img')
+  //       img.src = e.target.result as string
+  //       img.onload = function () {
+  //         const w = img.naturalWidth || img.width
+  //         const h = img.naturalHeight || img.height
+  //         setMediaDimensions(calculateDimensionsCover(w, h, dynamicWidth, STATIC_HEIGHT))
+  //         if (h >= w) {
+  //           setFit('horizontal-cover')
+  //         } else if (w > h) {
+  //           setFit('vertical-cover')
+  //         }
+  //       }
+  //       setFile(reader.result)
+  //     }
+  //     reader.readAsDataURL(f)
+  //   }
+  // }
 
   const reset = () => {
     setFile(null)
@@ -111,9 +111,9 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ src, cancel, onUpdate }) 
     setZoom(null)
   }
 
-  const onCropComplete = useCallback((_croppedArea, croppedAreaPixels) => {
-    setCroppedAreaPixels(croppedAreaPixels)
-  }, [])
+  // const onCropComplete = useCallback((_croppedArea, croppedAreaPixels) => {
+  //   setCroppedAreaPixels(croppedAreaPixels)
+  // }, [])
 
   const update = useCallback(async () => {
     try {
@@ -131,7 +131,7 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ src, cancel, onUpdate }) 
       <Box className={classes.container}>
         <Typography className={classes.title}>{i18n.t('common:profile.update_image')}</Typography>
         <div className={classes.cropContainer}>
-          {file ? (
+          {/* {file ? (
             <Cropper
               image={file}
               crop={crop}
@@ -156,7 +156,7 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ src, cancel, onUpdate }) 
                 <input {...getInputProps()} />
               </div>
             </div>
-          )}
+          )} */}
         </div>
         {file ? (
           <Box className={classes.controls}>
@@ -277,6 +277,7 @@ const useStyles = makeStyles<Theme, StyleProps>(() => ({
     position: 'relative',
     display: 'flex',
     width: ({ width }) => width,
+    backgroundColor: 'yellow',
     height: STATIC_HEIGHT,
   },
   touch: {
