8212a662b9654d8b58bcef1d6812bfdece7419b7
batjargal <dugardagva_batjargal@unimedia.co.jp>
2021-06-27 22:52:59 +0800

    try fix 18

--- src/components/ImagePicker/CoverSelector.tsx
+++ src/components/ImagePicker/CoverSelector.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback, useEffect } from 'react'
-import { Box, Typography, Slider, Link, Theme } from '@material-ui/core'
+import { Box, Typography, Link, Theme } from '@material-ui/core'
 import getCroppedImg from './Partials/cropImage'
 // import { calculateDimensionsCover } from './Partials/calculateDimensions'
 import ESDialog from '@components/Dialog'
@@ -7,11 +7,11 @@ import ButtonPrimary from '@components/ButtonPrimary'
 // import Image from 'next/image'
 // import { useDropzone } from 'react-dropzone'
 // import Cropper from 'react-easy-crop'
-import { Crop169 as RectIcon } from '@material-ui/icons'
-import ESLoader from '@components/Loader'
+// import { Crop169 as RectIcon } from '@material-ui/icons'
+// import ESLoader from '@components/Loader'
 import i18n from '@locales/i18n'
 // import { Colors } from '@theme/colors'
-import { makeStyles, withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import { useWindowDimensions } from '@utils/hooks/useWindowDimensions'
 
 interface CoverSelectorProps {
@@ -20,32 +20,32 @@ interface CoverSelectorProps {
   onUpdate: (file: File, blob: any) => void
 }
 
-const ImageSlider = withStyles({
-  root: {
-    color: '#E11AD4',
-    height: 8,
-  },
-  thumb: {
-    height: 24,
-    width: 24,
-    backgroundColor: '#fff',
-    border: '2px solid currentColor',
-    marginTop: -10,
-    '&:focus, &:hover, &$active': {
-      boxShadow: 'inherit',
-    },
-  },
-  active: {},
-  track: {
-    height: 4,
-    borderRadius: 2,
-  },
-  rail: {
-    height: 4,
-    borderRadius: 2,
-    color: '#FFFFFF30',
-  },
-})(Slider)
+// const ImageSlider = withStyles({
+//   root: {
+//     color: '#E11AD4',
+//     height: 8,
+//   },
+//   thumb: {
+//     height: 24,
+//     width: 24,
+//     backgroundColor: '#fff',
+//     border: '2px solid currentColor',
+//     marginTop: -10,
+//     '&:focus, &:hover, &$active': {
+//       boxShadow: 'inherit',
+//     },
+//   },
+//   active: {},
+//   track: {
+//     height: 4,
+//     borderRadius: 2,
+//   },
+//   rail: {
+//     height: 4,
+//     borderRadius: 2,
+//     color: '#FFFFFF30',
+//   },
+// })(Slider)
 
 const STATIC_WIDTH = 600
 const STATIC_HEIGHT = 200
@@ -55,19 +55,19 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ cancel, onUpdate }) => {
   const [file, setFile] = useState<any>(null)
   // const [crop, setCrop] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
-  const [zoom, setZoom] = useState<number>(1)
-  const [uploading, setUploading] = useState<boolean>(false)
+  // const [zoom, setZoom] = useState<number>(1)
+  // const [uploading, setUploading] = useState<boolean>(false)
   // const [fitType, setFit] = useState<'contain' | 'vertical-cover' | 'horizontal-cover'>('horizontal-cover')
   // const [mediaDimensions, setMediaDimensions] = useState<{ width: number; height: number }>({ width: STATIC_WIDTH, height: STATIC_HEIGHT })
   const { width: containerWidth } = useWindowDimensions(64)
   const [dynamicWidth, setDynamicWidth] = useState<number>(STATIC_WIDTH)
   const classes = useStyles({ width: dynamicWidth })
 
-  const userAgent = window.navigator.userAgent
+  // const userAgent = window.navigator.userAgent
   // console.log('CoverSelector.tsx 67 ', userAgent)
 
   useEffect(() => {
-    return setUploading(false)
+    // return setUploading(false)
   }, [])
 
   useEffect(() => {
@@ -130,50 +130,8 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ cancel, onUpdate }) => {
     <ESDialog open={true} title={i18n.t('common:profile.update_image')} handleClose={cancel} bkColor={'#2C2C2C'} alignTop={true}>
       <Box className={classes.container}>
         <Typography className={classes.title}>{i18n.t('common:profile.update_image')}</Typography>
-        <div className={classes.cropContainer}>
-          {/* {file ? (
-            <Cropper
-              image={file}
-              crop={crop}
-              zoom={zoom}
-              objectFit={fitType}
-              aspect={4 / 1}
-              style={{
-                containerStyle: { width: dynamicWidth, height: STATIC_HEIGHT, position: 'relative' },
-                mediaStyle: { width: mediaDimensions.width, height: mediaDimensions.height, position: 'relative' },
-              }}
-              showGrid={false}
-              onCropChange={setCrop}
-              onCropComplete={onCropComplete}
-              onZoomChange={setZoom}
-            />
-          ) : (
-            <div className={classes.imageContainer}>
-              {src ? <img src={src} className={classes.image} /> : <Image height="148" width="116" src="/images/big_logo.png" />}
-              <div className={classes.backdrop} />
-              <Camera fontSize="large" className={classes.camera} />
-              <div {...getRootProps()} className={classes.dropZone}>
-                <input {...getInputProps()} />
-              </div>
-            </div>
-          )} */}
-        </div>
-        {file ? (
-          <Box className={classes.controls}>
-            <RectIcon fontSize="small" className={classes.rect} />
-            <ImageSlider
-              value={zoom}
-              min={1}
-              max={3}
-              step={0.1}
-              aria-labelledby="Zoom"
-              onChange={(_, zoom) => setZoom(typeof zoom === 'object' ? zoom[0] : zoom)}
-            />
-            <RectIcon fontSize="small" className={classes.rect2} />
-          </Box>
-        ) : null}
-        <Typography className={classes.description}>{userAgent}</Typography>
-        {/* <Typography className={classes.description}>{i18n.t('common:messages.image_update')}</Typography> */}
+
+        <Typography className={classes.description}>{i18n.t('common:messages.image_update')}</Typography>
         <Box>
           <ButtonPrimary round gradient={false} onClick={cancel}>
             {i18n.t('common:common.cancel')}
@@ -185,14 +143,74 @@ const CoverSelector: React.FC<CoverSelectorProps> = ({ cancel, onUpdate }) => {
         <Link className={classes.link} onClick={reset}>
           {i18n.t('common:profile.reset')}
         </Link>
-
-        {uploading ? (
-          <Box className={classes.loader}>
-            <ESLoader />
-          </Box>
-        ) : null}
       </Box>
     </ESDialog>
+    // <ESDialog open={true} title={i18n.t('common:profile.update_image')} handleClose={cancel} bkColor={'#2C2C2C'} alignTop={true}>
+    //   <Box className={classes.container}>
+    //     <Typography className={classes.title}>{i18n.t('common:profile.update_image')}</Typography>
+    //     <div className={classes.cropContainer}>
+    //       {file ? (
+    //         <Cropper
+    //           image={file}
+    //           crop={crop}
+    //           zoom={zoom}
+    //           objectFit={fitType}
+    //           aspect={4 / 1}
+    //           style={{
+    //             containerStyle: { width: dynamicWidth, height: STATIC_HEIGHT, position: 'relative' },
+    //             mediaStyle: { width: mediaDimensions.width, height: mediaDimensions.height, position: 'relative' },
+    //           }}
+    //           showGrid={false}
+    //           onCropChange={setCrop}
+    //           onCropComplete={onCropComplete}
+    //           onZoomChange={setZoom}
+    //         />
+    //       ) : (
+    //         <div className={classes.imageContainer}>
+    //           {src ? <img src={src} className={classes.image} /> : <Image height="148" width="116" src="/images/big_logo.png" />}
+    //           <div className={classes.backdrop} />
+    //           <Camera fontSize="large" className={classes.camera} />
+    //           <div {...getRootProps()} className={classes.dropZone}>
+    //             <input {...getInputProps()} />
+    //           </div>
+    //         </div>
+    //       )}
+    //     </div>
+    //     {file ? (
+    //       <Box className={classes.controls}>
+    //         <RectIcon fontSize="small" className={classes.rect} />
+    //         <ImageSlider
+    //           value={zoom}
+    //           min={1}
+    //           max={3}
+    //           step={0.1}
+    //           aria-labelledby="Zoom"
+    //           onChange={(_, zoom) => setZoom(typeof zoom === 'object' ? zoom[0] : zoom)}
+    //         />
+    //         <RectIcon fontSize="small" className={classes.rect2} />
+    //       </Box>
+    //     ) : null}
+    //     <Typography className={classes.description}>{userAgent}</Typography>
+    //     {/* <Typography className={classes.description}>{i18n.t('common:messages.image_update')}</Typography> */}
+    // <Box>
+    //   <ButtonPrimary round gradient={false} onClick={cancel}>
+    //     {i18n.t('common:common.cancel')}
+    //   </ButtonPrimary>
+    //   <ButtonPrimary round onClick={update} style={{ marginLeft: 20 }} disabled={file === null || rawFile === null}>
+    //     {i18n.t('common:button.use')}
+    //   </ButtonPrimary>
+    // </Box>
+    // <Link className={classes.link} onClick={reset}>
+    //   {i18n.t('common:profile.reset')}
+    // </Link>
+
+    //     {uploading ? (
+    //       <Box className={classes.loader}>
+    //         <ESLoader />
+    //       </Box>
+    //     ) : null}
+    //   </Box>
+    // </ESDialog>
   )
 }
 
@@ -203,6 +221,36 @@ export interface StyleProps {
 }
 
 const useStyles = makeStyles<Theme, StyleProps>(() => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    // minHeight: 400,
+    marginTop: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gray',
+  },
+  title: {
+    marginTop: 40,
+    marginBottom: 40,
+  },
+  description: {
+    marginTop: 40,
+    marginBottom: 100,
+    maxWidth: 400,
+  },
+  link: {
+    color: '#FFFFFF30',
+    '&:focus': {
+      color: '#ffffff9c',
+    },
+    marginTop: 20,
+    marginBottom: 20,
+    cursor: 'pointer',
+    textDecoration: 'underline',
+  },
+  //----------------------------
   imageContainer: {
     display: 'flex',
     width: ({ width }) => width,
@@ -223,15 +271,6 @@ const useStyles = makeStyles<Theme, StyleProps>(() => ({
     justifyContent: 'center',
     alignItems: 'center',
     zIndex: 60,
-  },
-  link: {
-    color: '#FFFFFF30',
-    '&:focus': {
-      color: '#ffffff9c',
-    },
-    marginTop: 20,
-    cursor: 'pointer',
-    textDecoration: 'underline',
   },
   camera: {
     display: 'flex',
@@ -261,25 +300,6 @@ const useStyles = makeStyles<Theme, StyleProps>(() => ({
   rect2: {
     color: '#FFFFFF30',
     marginLeft: 10,
-  },
-  //----------------------------
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    minHeight: 400,
-    marginTop: 24,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    marginTop: 40,
-    marginBottom: 40,
-  },
-  description: {
-    marginTop: 40,
-    marginBottom: 120,
-    maxWidth: 400,
   },
   touch: {
     zIndex: 30,
