0f079324641ec194d40d47165d4ce51e4c3f6376
“khaynaa” <turmandakh_khayanhyaravaa@unimedia.co.jp>
2021-06-30 13:50:09 +0800

    changed

--- src/locales/jp/common.ts
+++ src/locales/jp/common.ts
@@ -770,7 +770,7 @@ export default {
     default_desc:
       '“人と人がつながる”ことで、ゲームはもっともっと楽しくなります。つながりが生む新たな体験と発見、そしてたくさんのプレイヤーとシーンを支えるファン達が交差するプラットフォーム。それがeXeLAB（エグゼラボ）です。',
     default_keywords: 'eXeLAB,エグゼラボ,game.ゲーム,ゲーマー,esports,eスポーツ,NTTe-sports,大会運営,ゲーム仲間,募集,コミュニティ',
-    home_top: 'eXeLAB',
+    home_top: 'eXeLAB｜ゲームが広がる。仲間ができる。',
     home_title: 'eXeLAB｜ホーム',
     arena_default_title: 'eXeLAB｜大会',
   },
