05300db8ae54389e54f0648465149668e298e813
“khaynaa” <turmandakh_khayanhyaravaa@unimedia.co.jp>
2021-06-30 13:26:23 +0800

    add arena list title

--- pages/arena/index.tsx
+++ pages/arena/index.tsx
@@ -1,11 +1,23 @@
 import MainLayout from '@layouts/MainLayout'
 import PageWithLayoutType from '@constants/page'
-
 import { ArenaHomeContainer } from '@containers/arena'
+import i18n from '@locales/i18n'
 const TournamentPage: PageWithLayoutType = () => {
   return <ArenaHomeContainer />
 }
 
 TournamentPage.Layout = MainLayout
+
+export async function getStaticProps(): Promise<{
+  props: {
+    title: string
+  }
+}> {
+  return {
+    props: {
+      title: i18n.t('common:page_head.arena_default_title'),
+    },
+  }
+}
 
 export default TournamentPage
