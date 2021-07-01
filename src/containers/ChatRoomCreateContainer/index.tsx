979ca0a89851c155e6141ab9da1c4f89d4a67f56
bayaroch <bayaroch@gmail.com>
2021-07-01 17:29:33 +0800

    scrollbar for room add input

--- src/containers/ChatRoomCreateContainer/index.tsx
+++ src/containers/ChatRoomCreateContainer/index.tsx
@@ -290,7 +290,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: 'none',
   },
   inputArea: {
-    pointerEvents: 'all',
+    pointerEvents: 'initial',
   },
   title: { fontSize: 17 },
   loaderBox: {
