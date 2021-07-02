32d1c728695f6ca664e52d8e88c37120fe8560f1
Nasanjargal <nasaa1997@gmail.com>
2021-07-02 16:25:47 +0800

    fix chip

--- src/components/GameSelector/GameList.tsx
+++ src/components/GameSelector/GameList.tsx
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 import { GameTitle } from '@services/game.service'
-import ESChip from './Chip'
+import ESChip from '@components/Chip'
 import { Box, Container, List, Typography } from '@material-ui/core'
 import i18n from '@locales/i18n'
 
@@ -45,6 +45,7 @@ const GameList: React.FC<GameListProps> = ({ games, selectedGames, handleAdd }) 
                 label={g.display_name}
                 onClick={() => handleAdd(g)}
                 color={checkIsSelected(g.id) ? 'primary' : 'default'}
+                isGameList={true}
               />
             ))}
           </List>
