5cffb2444954824d780a98100f5cb945e5b3676c
tsomone <tsolmon.nergui@unimedia.mn>
2021-07-05 19:27:11 +0800

    arena create co-organizer changes

--- src/locales/en/common.ts
+++ src/locales/en/common.ts
@@ -62,6 +62,7 @@ export default {
     confirm_back: '戻る',
     confirm_ok: 'キャンセルする',
     blocking: 'ブロック中',
+    username: 'ユーザー名',
   },
   page404: {
     title: '404',
@@ -518,7 +519,7 @@ export default {
     decide: '決定する',
     public: '公開',
     private: '限定公開',
-    co_orgonizer: '共同管理者を選ぶ',
+    choose_co_organizer: '共同管理者を選ぶ',
     user_hint: '指定できるのは相互フォローユーザーのみです',
     not_found: '一致するユーザーは見つかりませんでした',
     public_or_private: '大会公開設定',
