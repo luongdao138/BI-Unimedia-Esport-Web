8973425ce956fe6aa367275066d395bad2d88c83
Nasanjargal <nasaa1997@gmail.com>
2021-07-09 09:41:26 +0800

    fix report modal

--- src/containers/Report/index.tsx
+++ src/containers/Report/index.tsx
@@ -184,7 +184,13 @@ const ESReport: React.FC<ESReportProps> = ({ data, target_id, room_id, chat_id, 
           <BlankLayout>
             <Box pt={7.5} pb={9} className={classes.topContainer}>
               <Box py={2} display="flex" flexDirection="row" alignItems="center">
-                <IconButton className={classes.iconButtonBg} onClick={handleClose}>
+                <IconButton
+                  className={classes.iconButtonBg}
+                  onClick={() => {
+                    handleClose()
+                    formik.resetForm()
+                  }}
+                >
                   <Icon className="fa fa-arrow-left" fontSize="small" />
                 </IconButton>
                 <Box pl={2}>
