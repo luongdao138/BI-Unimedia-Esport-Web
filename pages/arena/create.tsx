3aecd392b3de55e4935279c798c57da71c710246
boomoo <tumenbat_enkhtsolmon@unimedia.co.jp>
2021-07-20 11:08:52 +0800

    arena create protected

--- pages/arena/create.tsx
+++ pages/arena/create.tsx
@@ -1,9 +1,20 @@
 import BlankLayout from '@layouts/BlankLayout'
 import PageWithLayoutType from '@constants/page'
 import ArenaCreateContainer from '@containers/arena/UpsertForm'
+import { getIsAuthenticated } from '@store/auth/selectors'
+import { useAppSelector } from '@store/hooks'
+import { useRouter } from 'next/router'
+import { ESRoutes } from '@constants/route.constants'
 import { GetStaticProps } from 'next'
 import i18n from '@locales/i18n'
+
 const ArenaCreatePage: PageWithLayoutType = () => {
+  const router = useRouter()
+  const isAuthenticated = useAppSelector(getIsAuthenticated)
+  if (!isAuthenticated) {
+    router.push(ESRoutes.TOP)
+    return <></>
+  }
   return <ArenaCreateContainer />
 }
 
