0e597abbab17799d6a1262fc3b00a918c3817b4d
Puujee <purevdalai.developer@gmail.com>
2021-07-20 17:56:37 +0800

    Fixed multiline and link bug

--- src/containers/arena/UpsertForm/Confirm.tsx
+++ src/containers/arena/UpsertForm/Confirm.tsx
@@ -164,7 +164,7 @@ const Confirm: React.FC<ConfirmProps> = ({ values, hardwares, prefectures, user,
       {formatDate(t('common:tournament_create.holding_period'), values.stepThree.start_date, values.stepThree.end_date)}
       <Box pb={2} />
       <ESInput labelPrimary={t('common:tournament_create.area')} value={areaName} disabled={true} fullWidth />
-      {values.stepThree.address && <ESInput value={values.stepThree.address} disabled={true} fullWidth />}
+      {values.stepThree.address && <ESInput value={values.stepThree.address} disabled={true} fullWidth multiline />}
       <Box pb={2} />
       <ESInput labelPrimary={t('common:tournament_create.organizer')} value={user.nickname} disabled={true} fullWidth />
       <Box pb={2} />
