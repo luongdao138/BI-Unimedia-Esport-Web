e7ff8e92748287c0e874c980afdb69a7c6ce7377
boomoo <tumenbat_enkhtsolmon@unimedia.co.jp>
2021-07-21 16:45:42 +0800

    added.

--- pages/settings/account/change-email/index.tsx
+++ pages/settings/account/change-email/index.tsx
@@ -1,8 +1,10 @@
 import AccountSettingsChangeEmailContainer from '@containers/Settings/Account/ChangeEmail'
 import BlankLayout from '@layouts/BlankLayout'
 import PageWithLayoutType from '@constants/page'
+import useAuthenticated from '@utils/hooks/useAuthenticated'
 
 const AccountChangeEmailPage: PageWithLayoutType = () => {
+  useAuthenticated()
   return <AccountSettingsChangeEmailContainer />
 }
 
--- pages/settings/account/confirm/index.tsx
+++ pages/settings/account/confirm/index.tsx
@@ -1,8 +1,10 @@
 import AccountSettingsConfirmContainer from '@containers/Settings/Account/Confirm'
 import BlankLayout from '@layouts/BlankLayout'
 import PageWithLayoutType from '@constants/page'
+import useAuthenticated from '@utils/hooks/useAuthenticated'
 
 const AccountChangeEmailConfirmPage: PageWithLayoutType = () => {
+  useAuthenticated()
   return <AccountSettingsConfirmContainer />
 }
 
