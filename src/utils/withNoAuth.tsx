e1db0adcb53ef1e9f41008f8985c0384e419ce9d
Tsengel <enkhbat.tsengel@gmail.com>
2021-07-30 11:08:50 +0800

    issue/46-Auth HOC fix

--- src/utils/withNoAuth.tsx
+++ src/utils/withNoAuth.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { ESRoutes } from '@constants/route.constants'
-import { getIsAuthenticated } from '@store/auth/selectors'
+import { getHasEmail, getIsAuthenticated } from '@store/auth/selectors'
 import { useAppSelector } from '@store/hooks'
 import { useRouter } from 'next/router'
 import React, { ComponentType, useState, useEffect } from 'react'
@@ -8,13 +8,14 @@ import React, { ComponentType, useState, useEffect } from 'react'
 export function withNoAuth<T extends object>(Component: ComponentType<T>, fallback?: string): React.FC {
   const AppWithAuth: React.FC<T> = (props) => {
     const isAuth = useAppSelector(getIsAuthenticated)
+    const hasEmail = useAppSelector(getHasEmail)
     const router = useRouter()
     const [render, setRender] = useState(false)
 
     useEffect(() => {
       if (isAuth) {
         router.push(ESRoutes.HOME)
-      } else if (fallback) {
+      } else if (!hasEmail && fallback) {
         router.push(fallback)
       } else {
         setRender(true)
