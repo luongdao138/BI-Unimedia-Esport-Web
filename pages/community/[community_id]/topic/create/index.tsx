4efb94102c6a56bd4a283f9d00e70c689f5c81a4
Tamir <tamir.naranbaatar@unimedia.mn>
2021-08-12 15:47:06 +0800

    fix

--- pages/community/[community_id]/topic/create/index.tsx
+++ pages/community/[community_id]/topic/create/index.tsx
@@ -1,6 +1,5 @@
 import BlankLayout from '@layouts/BlankLayout'
 import PageWithLayoutType from '@constants/page'
-
 import TopicCreateContainer from '@containers/Community/Topic/UpsertForm'
 
 const TopicCreatePage: PageWithLayoutType = () => {
