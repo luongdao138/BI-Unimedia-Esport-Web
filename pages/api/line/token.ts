fce3184c7c6380dbbae418d0f2d1eefd00170e15
Tsengel <enkhbat.tsengel@gmail.com>
2021-08-13 19:08:11 +0800

    issue/130-fix-line-login

--- pages/api/line/token.ts
+++ pages/api/line/token.ts
@@ -1,18 +1,10 @@
+import getLineAccessToken, { GetLineAccessTokenResponse } from '@utils/helpers/getLineAccessToken'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
 
-type Data = {
-  access_token?: string
-  error?: string
-}
-
-export default async (req: NextApiRequest, res: NextApiResponse<Data>): Promise<void> => {
+export default async (req: NextApiRequest, res: NextApiResponse<GetLineAccessTokenResponse>): Promise<void> => {
   try {
-    if (req.method === 'POST') {
-      const { data } = await axios.post<Data>(
-        `https://api.line.me/oauth2/v2.1/token`,
-        `grant_type=authorization_code&code=${req.body.code}&redirect_uri=${process.env.NEXT_PUBLIC_LINE_CALLBACK}&client_id=${process.env.NEXT_PUBLIC_LINE_CLIENT_ID}&client_secret=${process.env.LINE_CLIENT_SECRET}`
-      )
+    if (req.method === 'POST' && req.body.code) {
+      const data = await getLineAccessToken(req.body.code)
       res.status(200).json(data)
     } else {
       res.status(404).json({ error: 'not_found' })
