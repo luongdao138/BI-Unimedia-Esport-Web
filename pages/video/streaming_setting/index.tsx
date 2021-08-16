d0828a5d86070292de017b0dd83dab4969fa4377
sondt <sondt1610@gmail.com>
2021-08-16 16:33:23 +0700

    display side menu by role

--- pages/video/streaming_setting/index.tsx
+++ pages/video/streaming_setting/index.tsx
@@ -1,11 +1,15 @@
 import StreamLayout from '@layouts/StreamLayout'
 import PageWithLayoutType from '@constants/page'
 import StreamingSettingContainer from '@containers/StreamingSettingContainer'
+import { useRouter } from 'next/router'
 
 const StreamingSettingPage: PageWithLayoutType = () => {
+  const router = useRouter()
+  const default_tab = router?.query?.default_tab || 0
+  
   return (
-    <StreamLayout loginRequired>
-      <StreamingSettingContainer />
+    <StreamLayout>
+      <StreamingSettingContainer default_tab={Number(default_tab)}/>
     </StreamLayout>
   )
 }
