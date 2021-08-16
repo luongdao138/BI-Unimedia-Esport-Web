ea0ca77468bba9b9fa4fd02d3dcf14be562f232e
bayaroch <bayaroch@gmail.com>
2021-08-16 15:37:42 +0800

    action dispatch init

--- src/containers/lobby/hooks/useLobbyActions.tsx
+++ src/containers/lobby/hooks/useLobbyActions.tsx
@@ -1,6 +1,6 @@
 import * as actions from '@store/lobby/actions'
 import { createMetaSelector } from '@store/metadata/selectors'
-import { useAppSelector } from '@store/hooks'
+import { useAppSelector, useAppDispatch } from '@store/hooks'
 import { Meta } from '@store/metadata/actions/types'
 
 const entryMetaSelector = createMetaSelector(actions.entryLobby)
@@ -11,14 +11,30 @@ const useLobbyActions = (): {
   entryMeta: Meta
   cancelMeta: Meta
   unjoinMeta: Meta
+  entry: (id: number) => void
+  cancel: (id: number) => void
+  unjoin: (id: number) => void
 } => {
+  const dispatch = useAppDispatch()
   const entryMeta = useAppSelector(entryMetaSelector)
   const cancelMeta = useAppSelector(cancelMetaSelector)
   const unjoinMeta = useAppSelector(unjoinMetaSelector)
+  const entry = (id: number) => {
+    dispatch(actions.entryLobby(id))
+  }
+  const cancel = (id: number) => {
+    dispatch(actions.cancelLobby(id))
+  }
+  const unjoin = (id: number) => {
+    dispatch(actions.unjoinLobby(id))
+  }
   return {
     entryMeta,
     cancelMeta,
     unjoinMeta,
+    entry,
+    cancel,
+    unjoin,
   }
 }
 
