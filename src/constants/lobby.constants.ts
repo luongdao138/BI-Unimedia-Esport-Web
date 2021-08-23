73bc3b6bc3b4ae0c570c940a9c6fbd49d8b71525
ums-211 <ums-211@ums-211s-MacBook-Pro.local>
2021-08-23 22:54:53 +0800

    Added dialog translations

--- src/constants/lobby.constants.ts
+++ src/constants/lobby.constants.ts
@@ -63,3 +63,34 @@ export enum MAIN_ACTIONS {
   CANCEL = 3,
   MEMBER_CONFIRM = 4,
 }
+
+export const LOBBY_DIALOGS = {
+  ENTRY_CONFIRMATION: {
+    title: 'lobby.entry_confirmation_dialog.title',
+    desc: 'lobby.entry_confirmation_dialog.desc',
+    warningText: 'lobby.entry_confirmation_dialog.warningText',
+    confirmationText: 'lobby.entry_confirmation_dialog.confirmationText',
+    cancelationText: 'lobby.entry_confirmation_dialog.cancelationText',
+  },
+  DECLINE_ENTRY: {
+    title: 'lobby.decline_entry_dialog.title',
+    desc: 'lobby.decline_entry_dialog.desc',
+    warningText: 'lobby.decline_entry_dialog.warningText',
+    confirmationText: 'lobby.decline_entry_dialog.confirmationText',
+    cancelationText: 'lobby.decline_entry_dialog.cancelationText',
+  },
+  CONFIRM_MEMBER: {
+    title: 'lobby.confirm_member_dialog.title',
+    desc: 'lobby.confirm_member_dialog.desc',
+    warningText: 'lobby.confirm_member_dialog.warningText',
+    confirmationText: 'lobby.confirm_member_dialog.confirmationText',
+    cancelationText: 'lobby.confirm_member_dialog.cancelationText',
+  },
+  ENTRY_DEADLINE: {
+    title: 'lobby.entry_deadline_dialog.title',
+    desc: 'lobby.entry_deadline_dialog.desc',
+    warningText: 'lobby.entry_deadline_dialog.warningText',
+    confirmationText: 'lobby.entry_deadline_dialog.confirmationText',
+    cancelationText: 'lobby.entry_deadline_dialog.cancelationText',
+  },
+}
