96e0a0c9fc975c415f58f6cae8b9b4e2830f1cf7
bayaroch <bayaroch@gmail.com>
2021-08-31 13:12:05 +0800

    fix

--- src/containers/arena/UpsertForm/Partials/CoverUploader.tsx
+++ src/containers/arena/UpsertForm/Partials/CoverUploader.tsx
@@ -64,16 +64,12 @@ const CoverUploader: React.FC<ProfileAvatarProps> = ({
         }}
       >
         {localSrc.toString() !== '' && <img className={classes.image} src={localSrc.toString()} />}
-
         <Box display="flex" flexDirection="column" alignItems="center" position="absolute" zIndex="100" className={classes.logoWhite}>
           <Camera fontSize="large" className={classes.camera} />
           <Typography>{t('common:tournament.cover_upload_select_img')}</Typography>
         </Box>
-
         <img src="/images/logo.svg" className={classes.logo} />
-
         <div className={classes.outerBackdrop} />
-
         {drag || isUploading ? <div className={classes.backdrop} /> : null}
         {isUploading ? (
           <Box className={classes.loader}>
@@ -101,12 +97,21 @@ const useStyles = makeStyles(() => ({
   },
   logoWhite: {
     color: Colors.text[200],
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    left: 0,
+    right: 0,
   },
   image: {
     position: 'absolute',
     zIndex: 30,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
     width: '100%',
-    height: 'auto',
+    height: '100%',
     objectFit: 'contain',
   },
   touch: {
@@ -114,11 +119,11 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     position: 'relative',
     overflow: 'hidden',
-    height: 120,
     alignItems: 'center',
     justifyContent: 'center',
     border: `1px dashed ${Colors.text[200]}`,
     borderRadius: 4,
+    paddingTop: '30.21756647864625%',
     '&:hover': {
       cursor: 'pointer',
     },
