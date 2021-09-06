2bf4dfb0398f104d8afac18de1b2aded5f77458c
bayaroch <bayaroch@gmail.com>
2021-09-06 17:32:53 +0800

    ga change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,12 +20,11 @@ class Document extends NextDocument<Props> {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${GA_TRACKING_ID}');
           `,
             }}
           />
