0b9db3320c2c020dce073922a71f20199ff7fe13
Tsengel <enkhbat.tsengel@gmail.com>
2021-09-06 11:16:17 +0800

    issue/225-login redirect fix

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -3,13 +3,11 @@ import PageWithLayoutType from '@constants/page'
 import MainLayout from '@layouts/MainLayout'
 
 const ProfilePage: PageWithLayoutType = () => {
-  return <ProfileContainer />
-}
-
-ProfilePage.Layout = MainLayout
-
-MainLayout.defaultProps = {
-  loginRequired: true,
+  return (
+    <MainLayout loginRequired={true}>
+      <ProfileContainer />
+    </MainLayout>
+  )
 }
 
 export default ProfilePage
