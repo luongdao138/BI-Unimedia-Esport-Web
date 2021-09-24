74c5751563115a1bf6097185f02246f747510539
bilguun <bilguun.davaadorj@gmail.com>
2021-09-24 19:04:47 +0800

    i18 delete

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -88,20 +88,6 @@ i18n
             default:
               return ''
           }
-        } else if (format === 'battle_royale_rule') {
-          switch (value) {
-            case 'score_attack': {
-              return 'スコア'
-            }
-            case 'time_attack': {
-              return 'タイム'
-            }
-            case '': {
-              return '順位'
-            }
-            default:
-              return ''
-          }
         }
         return value
       },
