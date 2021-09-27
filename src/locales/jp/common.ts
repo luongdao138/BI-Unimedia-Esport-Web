fb5a0adab80f0527b8ed755ce1d04465c863db49
Wolfy <tamir.naranbaatar@unimedia.mn>
2021-09-27 14:09:43 +0800

    fixing

--- src/locales/jp/common.ts
+++ src/locales/jp/common.ts
@@ -245,6 +245,7 @@ export default {
     report: '通報',
     reported_community: 'コミュニティを通報しました',
     report_community: 'コミュニティを通報する',
+    community_to_report: '通報するコミュニティ',
     community_id: 'コミュニティID：',
     copy_shared_url: '共有URLをコピー',
     community_results: 'コミュニティ「:key」 の検索結果',
