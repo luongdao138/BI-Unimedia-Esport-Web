c7be38f8a4ff3f3e16ff09908b939d93e8decee4
tsomone <tsolmon.nergui@unimedia.mn>
2021-09-30 19:36:07 +0800

    #290 fix

--- src/containers/Notifications/notificationBadgeItem.tsx
+++ src/containers/Notifications/notificationBadgeItem.tsx
@@ -17,10 +17,8 @@ const NotificationBadgeItem: React.FC<Props> = ({ data }) => {
         return <ESAvatar src={'/images/avatar.png'} />
       case NOTIFICATION_ACTION_TYPES.NOTIFICATION_TYPE_FOLLOW:
         return <ESAvatar alt={notification.nickname} src={notification.avatar_url} />
-      case NOTIFICATION_ACTION_TYPES.NOTIFICATION_TYPE_RECRUITMENT ||
-        NOTIFICATION_ACTION_TYPES.NOTIFICATION_TYPE_RECRUITMENT_ACTION_CANCELED ||
-        NOTIFICATION_ACTION_TYPES.NOTIFICATION_TYPE_RECRUITMENT_ACTION_SELECTED:
-        return <ESAvatar src={'/images/avatar.png'} />
+      case NOTIFICATION_ACTION_TYPES.NOTIFICATION_TYPE_RECRUITMENT:
+        return <ESAvatar src={notification.avatar_url || '/images/avatar.png'} />
       default:
         return <ESAvatar src={notification.avatar_url || '/images/avatar.png'} />
     }
