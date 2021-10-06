33eea2a478b1efcb6615ddc3ed628ae4137fbd63
Tsengel <enkhbat.tsengel@gmail.com>
2021-10-06 09:26:53 +0800

    battle-royal/change score field

--- src/containers/arena/Winners/index.tsx
+++ src/containers/arena/Winners/index.tsx
@@ -133,12 +133,12 @@ const ArenaWinners: React.FC = () => {
             {arenaBRWinners.map((p, idx) => (
               <ResultListItem
                 key={idx}
-                position={idx + 1}
+                position={p.attributes.position}
                 avatar={<Avatar src={p.attributes.avatar_url} alt={p.attributes.name} />}
                 onClickAvatar={() => selectBRParticipant(p)}
                 name={p.attributes.team ? p.attributes.team.data.attributes.name : p.attributes.name}
                 nameSecondary={p.attributes.user ? `@${p.attributes.user.user_code}` : ''}
-                score={p.attributes.position}
+                score={p.attributes.attack_score}
               />
             ))}
           </ResultList>
