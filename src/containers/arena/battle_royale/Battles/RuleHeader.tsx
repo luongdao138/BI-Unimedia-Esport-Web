90260fe9c4afc48e6155d509b23358b1b3127909
Enkhtulga <gantsagaan_enkhtulga@unimedia.co.jp>
2021-10-07 09:19:08 +0800

    ng fixes

--- src/containers/arena/battle_royale/Battles/RuleHeader.tsx
+++ src/containers/arena/battle_royale/Battles/RuleHeader.tsx
@@ -1,6 +1,7 @@
 import { BoxProps, Box, Typography, makeStyles } from '@material-ui/core'
 import { TournamentRule } from '@services/arena.service'
 import i18n from '@locales/i18n'
+import { Colors } from '@theme/colors'
 
 const RuleTitles: Record<TournamentRule, string> = {
   battle_royale: i18n.t('common:arena.rules_title.battle_royale'),
@@ -10,12 +11,21 @@ const RuleTitles: Record<TournamentRule, string> = {
   double: '',
 }
 
+const RuleErrors: Record<TournamentRule, string> = {
+  battle_royale: i18n.t('common:arena.rules_title.battle_royale_error'),
+  score_attack: i18n.t('common:arena.rules_title.score_attack_error'),
+  time_attack: i18n.t('common:arena.rules_title.time_attack_error'),
+  single: '',
+  double: '',
+}
+
 interface RuleHeaderProps extends BoxProps {
   rule: TournamentRule
   showCaution: boolean
+  showError: boolean
 }
 
-const RuleHeader: React.FC<RuleHeaderProps> = ({ rule, showCaution, ...props }) => {
+const RuleHeader: React.FC<RuleHeaderProps> = ({ rule, showCaution, showError, ...props }) => {
   const classes = useStyles()
 
   return (
@@ -26,6 +36,9 @@ const RuleHeader: React.FC<RuleHeaderProps> = ({ rule, showCaution, ...props }) 
           {i18n.t('common:arena.enter_own_score')}
         </Typography>
       ) : null}
+      <Box textAlign="center" pb={3}>
+        {showError ? <Typography style={{ color: Colors.secondary, paddingTop: 4 }}>{RuleErrors[rule]}</Typography> : null}
+      </Box>
     </Box>
   )
 }
