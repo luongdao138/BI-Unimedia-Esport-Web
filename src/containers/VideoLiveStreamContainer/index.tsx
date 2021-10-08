d6f2a96fcaac93cddc782f3d01f12971e7add686
landt7262 <landt7262@co-well.com.vn>
2021-10-08 17:41:48 +0700

    Update video hls

--- src/containers/VideoLiveStreamContainer/index.tsx
+++ src/containers/VideoLiveStreamContainer/index.tsx
@@ -10,7 +10,7 @@ import DistributorInfo from './DistributorInfo'
 import ProgramInfo from './ProgramInfo'
 import RelatedVideos from './RelatedVideos'
 import ChatContainer from './ChatContainer'
-import LiveStreamContent from './LiveStreamContent'
+// import LiveStreamContent from './LiveStreamContent'
 import DonatePoints from './DonatePoints'
 import DonatePointsConfirmModal from './DonatePointsConfirmModal/DonatePointsConfirmModal'
 import ProgramInfoNoViewingTicket from '@containers/VideoLiveStreamContainer/ProgramInfoNoViewingTicket'
@@ -38,6 +38,7 @@ import { EVENT_LIVE_STATUS, LIVE_VIDEO_TYPE } from '@constants/common.constants'
 import DialogLoginContainer from '@containers/DialogLogin'
 import _ from 'lodash'
 import { useWindowDimensions } from '@utils/hooks/useWindowDimensions'
+import LiveStreamContent from './LiveStreamContentDemo'
 
 enum TABS {
   PROGRAM_INFO = 1,
