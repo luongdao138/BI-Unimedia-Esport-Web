2ec2479c9b42026c629d7f9fbde49f8665bc03a4
landt7262 <landt7262@co-well.com.vn>
2021-10-13 09:02:59 +0700

    update time live

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -108,12 +108,12 @@ const App = ({ Component, pageProps }: Props) => {
 
   const { previousRoute } = useRouteUrlHistory()
   const handleLoadScript = () => {
-    console.warn('IVSPlayer; ', window?.IVSPlayer)
+    console.warn('https://cdn.jsdelivr.net/npm/hls.js@latest ', window?.IVSPlayer)
   }
   return (
     <>
       <ESHead title={pageProps.title || 'eXeLAB'} desc={pageProps.desc} keywords={pageProps.keywords} image={pageProps.image} />
-      <Script url="https://player.live-video.net/1.5.0/amazon-ivs-player.min.js" onError={console.error} onLoad={handleLoadScript} />
+      {/* <Script url="https://player.live-video.net/1.5.0/amazon-ivs-player.min.js" onError={console.error} onLoad={handleLoadScript} /> */}
       <Script url="https://cdn.jsdelivr.net/npm/hls.js@latest" onError={console.error} onLoad={handleLoadScript} />
       <PersistGate persistor={persistStore(store)}>
         <RouteContext.Provider
