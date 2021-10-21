13e0f56e93c4f3e431a6b15ff938bdcd52a03874
Enkhtulga <gantsagaan_enkhtulga@unimedia.co.jp>
2021-10-21 10:15:53 +0800

    fix

--- src/locales/en/common.ts
+++ src/locales/en/common.ts
@@ -271,6 +271,7 @@ export default {
       placeholder: 'キーワードからトピックを検索します',
       result: '検索結果',
       no_data: '該当するトピックは見つかりませんでした',
+      content_text: 'トピック本文　',
     },
     follow_list: 'フォローリスト',
     member_list: {
