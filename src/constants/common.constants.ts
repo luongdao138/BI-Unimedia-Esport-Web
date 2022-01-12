01e9ecea449ebba8fea881d33b0d81159132eeb7
cuonghq7271 <cuonghq7271@co-well.com.vn>
2022-01-12 14:21:54 +0700

    Update input type of title field on inquiry screen

--- src/constants/common.constants.ts
+++ src/constants/common.constants.ts
@@ -344,3 +344,12 @@ export const EVENT_STATE_CHANNEL = {
 }
 
 export const CONFIRM_SETTING_DELAY = 20 * 1000
+
+export const INQUIRY_REQUEST_LABELS = [
+  'inquiry.label_about_arena',
+  'inquiry.label_about_lobby',
+  'inquiry.label_about_community',
+  'inquiry.label_about_watching_video',
+  'inquiry.label_about_deletion',
+  'inquiry.label_others',
+]
