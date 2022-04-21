31e59d89ae7664adad2415eb7600d1cb8bb645da
aitx7270-cowell <aitx7270@co-well.vn>
2022-04-21 23:18:06 +0700

    fix lag ranking tab and chat tab

--- src/containers/VideoLiveStreamContainer/ChatContainer/index.tsx
+++ src/containers/VideoLiveStreamContainer/ChatContainer/index.tsx
@@ -76,7 +76,6 @@ import { useCheckDisplayChat } from '@utils/hooks/useCheckDisplayChat'
 import { VideoContext } from '../VideoContext'
 import TabsContainer from './components/TabsContainer'
 import ChatLoader from './components/ChatLoader'
-import { useVideoTabContext } from '../VideoContext/VideTabContext'
 import ChatMessages from './components/ChatMessages'
 import { useRect } from '@utils/useRect'
 
@@ -291,7 +290,8 @@ const ChatContainer: React.FC<ChatContainerProps> = forwardRef(
     const [videoTimeIsRewinding, setVideoTimeIsRewinding] = useState(0)
 
     const isSwitchingTabRef = useRef(false)
-    const { activeSubTab, activeTab, setActiveTab, setActiveSubTab } = useVideoTabContext()
+    const [activeTab, setActiveTab] = useState<number>(VIDEO_TABS.CHAT)
+    const [activeSubTab, setActiveSubTab] = useState<number>(SUB_TABS.MESS.ALL)
 
     const { selectors } = userProfileStore
 
